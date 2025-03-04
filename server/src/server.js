bd22a0e05ecf34c5a854da74b1d274a1094d5ced
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-04 19:05:14 -0300

    :sparkles: feat: Uso da rota no arquivo server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const database = require("./config/database");
 const cors = require("cors");
+const routerAuth = require("./routes/auth");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", routerAuth);
 
 database.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
