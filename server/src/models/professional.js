e7e512da26f383b5339022f74fbb903d0db8d6fa
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-04 17:17:47 -0300

    :sparkles: feat: Criei o modelo da tabela profissionais

---
+++ server/src/models/professional.js
@@ -0,0 +1,31 @@
+const database = require("../config/database");
+const { DataTypes } = require("sequelize");
+
+const Professional = database.define("professionals", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
+module.exports = Professional;
