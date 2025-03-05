2b4d997be7f9e23e0c68c58cd517e6cd3c39a02f
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-05 09:01:11 -0300

    :wastebasket: remove: Removi o createdAt e updatedAt que não vão ser usados

--- server/src/models/professional.js
+++ server/src/models/professional.js
@@ -1,31 +1,29 @@
 const database = require("../config/database");
 const { DataTypes } = require("sequelize");
 
-const Professional = database.define("professionals", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Professional = database.define(
+  "professionals",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 module.exports = Professional;
